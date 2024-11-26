534376da6ee9f701651e7f65d0e4c6d2cff09911
Santi <Santi@DESKTOP-IG9DTLS>
2024-11-25 21:05:41 -0300

    Create Customer class

---
+++ Clases/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountTransactionManagement
+{
+    internal class Customer
+    {
+        private string name;
+        private string id;
+        private string address;
+        private string phoneNumber;
+
+        public Customer(string name, string id, string address, string phoneNumber) 
+        {
+            this.name = name;
+            this.id = id;
+            this.address = address;
+            this.phoneNumber = phoneNumber;
+        }
+
+        public string Name { get { return name; } }
+        public string Id { get { return id; } } 
+        public string Address { get { return address; } }
+        public string PhoneNumber { get { return phoneNumber; } }
+    }
+}
